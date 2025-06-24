eb69e7b5a35c437b0687a6ab6573c9996a6bd8d1
Peter Cowal <pcowal@gmail.com>
2025-06-24 10:25:33 -0700

    bypasses tesseract image :)

--- scripts/tesseract_test.py
+++ scripts/tesseract_test.py
@@ -1,11 +1,17 @@
+import sys
+sys.path.append("src")
+sys.path.append("tesseracts")
+
 import numpy as np
 
 from tesseract_core import Tesseract
 
+import sheaths.vlasov.tesseract_api as vlasov_sheath_tesseract_api
+
 inputs = {"n": 6e22, "T": 20.0, "Vp": 1500.0, "Lz": 0.5 }
 
 
-with Tesseract.from_image("vlasov_sheath") as sheath_tx:
+with Tesseract.from_tesseract_api(vlasov_sheath_tesseract_api) as sheath_tx:
     result = sheath_tx.apply(inputs)
 
 print(result)
